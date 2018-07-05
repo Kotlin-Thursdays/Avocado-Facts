0860f536cf781b6d1c6403e27698dcc37f13dcd2
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2018-07-05 07:58:45 -0500

    Main Activity files

--- app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
+++ app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.amandahinchman.avocadofacts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // Declare our View Variables
+    private lateinit var factTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Assign the Views from the layout
+        factTextView = findViewById(R.id.avocadoFact)
+
     }
 }
