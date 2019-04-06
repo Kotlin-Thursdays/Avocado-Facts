150174151ac5c4f8c65add61042cc80221185d43
uddyami <uddyami.agrawal@gmail.com>
2019-04-06 13:54:30 -0500

    add more button with random generator UI: add more button Functionality: random generator to pick fact from array and display Everything is done on Main Thread.

--- app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
+++ app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.amandahinchman.avocadofacts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     // Declare our View Variables
     private lateinit var factTextView: TextView
+
+    private val facts = arrayOf("fact 1",
+            "fact 2",
+            "fact 3",
+            "fact 4",
+            "fact 5",
+            "fact 6",
+            "fact 7",
+            "fact 8",
+            "fact 9",
+            "fact 10")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +28,15 @@ class MainActivity : AppCompatActivity() {
 
         // Assign the Views from the layout
         factTextView = findViewById(R.id.avocadoFact)
+
+        var moarButton = findViewById<Button>(R.id.more)
+        moarButton.setOnClickListener {
+            val randomGen= Random()
+            val randomNumber= randomGen.nextInt(10)
+            factTextView.text = facts.get(randomNumber)
+        }
+
+
 
     }
 }
