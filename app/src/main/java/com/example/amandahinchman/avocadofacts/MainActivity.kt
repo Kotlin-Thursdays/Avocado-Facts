27b9f50c5dabd2da7432638f71fa4eb24f99a390
uddyami <uddyami.agrawal@gmail.com>
2019-04-22 18:12:59 -0500

    anko is here

--- app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
+++ app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.amandahinchman.avocadofacts
 
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import java.lang.ref.WeakReference
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        const val TAG: String = "MainActivity"
+    }
     // Declare our View Variables
     private lateinit var factTextView: TextView
 
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         val moarButton = findViewById<Button>(R.id.more)
         moarButton.setOnClickListener {
-            NewAvocadoFactTask(this).execute()
+            getRandomFact()
         }
 
     }
@@ -32,45 +34,28 @@ class MainActivity : AppCompatActivity() {
         factTextView.text = fact
     }
 
-    /**
-     * AsyncTask<Params, Progress, Result> signature interface:
-     * - params: the type of the parameters sent to the task upon
-     *             execution
-     * - progress: the type of the progress units published during the
-     *             background computation
-     * - result: the type of the result of the background computation
-     */
-    private class NewAvocadoFactTask(activity: MainActivity) : AsyncTask<Void, Void, String>() {
-        var reference: WeakReference<MainActivity> = WeakReference(activity)
 
-        companion object {
-            const val TAG: String = "NewAvocadoFactTask"
-        }
-        private val facts = arrayOf("fact 1",
-                "fact 2",
-                "fact 3",
-                "fact 4",
-                "fact 5",
-                "fact 6",
-                "fact 7",
-                "fact 8",
-                "fact 9",
-                "fact 10")
+    private val facts = arrayOf("fact 1",
+            "fact 2",
+            "fact 3",
+            "fact 4",
+            "fact 5",
+            "fact 6",
+            "fact 7",
+            "fact 8",
+            "fact 9",
+            "fact 10")
 
-        override fun doInBackground(vararg params: Void?): String {
-            Log.v(TAG, "doInBackground is running on ${Thread.currentThread().name}")
-
+    private fun getRandomFact(){
+        doAsync{
             val randomGen = Random()
             val randomNumber = randomGen.nextInt(10)
-            return facts[randomNumber]
-        }
-
-        override fun onPostExecute(result: String?) {
-            Log.v(TAG, "onPostExecute is running on ${Thread.currentThread().name}")
-            super.onPostExecute(result)
-            result?.let {
-                reference.get()?.updateAvocadoFact(it)  // if reference.get is not null execute the function
-            }
+            val fact =facts[randomNumber]
+            Log.v(MainActivity.TAG, "doAsync is running on ${Thread.currentThread().name}")
+          uiThread {
+              updateAvocadoFact(fact)
+              Log.v(MainActivity.TAG, "uiThread is running on ${Thread.currentThread().name}")
+          }
         }
     }
 }
