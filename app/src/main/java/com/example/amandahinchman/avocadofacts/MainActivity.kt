0f0a682626d1ea422348c80e2c1b33a5c020a7db
uddyami agrawal <uddyami@users.noreply.github.com>
2019-04-22 23:59:47 -0500

    add facts (#2)
    * add facts
    
    * moar facts

--- app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
+++ app/src/main/java/com/example/amandahinchman/avocadofacts/MainActivity.kt
@@ -22,55 +22,35 @@ class MainActivity : AppCompatActivity() {
         factTextView = findViewById(R.id.avocadoFact)
 
         val moarButton = findViewById<Button>(R.id.more)
-        moarButton.setOnClickListener {
-            NewAvocadoFactTask(this).execute()
-        }
-
+        moarButton.setOnClickListener { NewAvocadoFactTask(this).execute() }
     }
 
-    fun updateAvocadoFact(fact: String) {
-        factTextView.text = fact
-    }
+    fun updateAvocadoFact(fact: String) { factTextView.text = fact }
 
-    /**
-     * AsyncTask<Params, Progress, Result> signature interface:
-     * - params: the type of the parameters sent to the task upon
-     *             execution
-     * - progress: the type of the progress units published during the
-     *             background computation
-     * - result: the type of the result of the background computation
-     */
     private class NewAvocadoFactTask(activity: MainActivity) : AsyncTask<Void, Void, String>() {
         var reference: WeakReference<MainActivity> = WeakReference(activity)
 
         companion object {
             const val TAG: String = "NewAvocadoFactTask"
         }
-        private val facts = arrayOf("fact 1",
-                "fact 2",
-                "fact 3",
-                "fact 4",
-                "fact 5",
-                "fact 6",
-                "fact 7",
-                "fact 8",
-                "fact 9",
-                "fact 10")
 
         override fun doInBackground(vararg params: Void?): String {
             Log.v(TAG, "doInBackground is running on ${Thread.currentThread().name}")
 
             val randomGen = Random()
-            val randomNumber = randomGen.nextInt(10)
-            return facts[randomNumber]
+            val randomNumber = randomGen.nextInt(getFacts().size)
+            return getFacts()[randomNumber]
         }
 
         override fun onPostExecute(result: String?) {
             Log.v(TAG, "onPostExecute is running on ${Thread.currentThread().name}")
             super.onPostExecute(result)
             result?.let {
-                reference.get()?.updateAvocadoFact(it)  // if reference.get is not null execute the function
+                reference.get()?.updateAvocadoFact(it)
+                // if reference.get is not null execute the function
             }
         }
+
     }
+
 }
